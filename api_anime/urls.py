931f2c065589200054a9fa47e94a7475e8fd2ab3
Kutman73 <0.0000000002clash@gmail.com>
2023-04-10 16:15:57 +0600

    refactoring urls

--- api_anime/urls.py
+++ api_anime/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path('api/v1/', include('anime.api.urls'))
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
