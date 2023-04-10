ff5b1ac3773df6faa071efc7913ede9cacf50917
Kutman73 <0.0000000002clash@gmail.com>
2023-04-10 15:16:40 +0600

    first initial

---
+++ api_anime/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.conf.urls.static import static
+from api_anime import settings
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('anime.api.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
